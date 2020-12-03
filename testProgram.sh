d5324a40e364febf188cd27968d0d9f10cd4a4a8
Mary Brown <mbrown306@WFIL091.cscc.local>
2020-12-02 22:43:44 -0500

    changes for count

--- testProgram.sh
+++ testProgram.sh
@@ -4,6 +4,16 @@
 #Fill in the unit test for program.sh described in the lab assignment below
 
 #Check that the result matches the correct result
-
-#Return a non-zero exit code if the test does not pass
-exit 1
+sum=0
+for num in $(cat even-number)
+do
+   ((sum+=num))
+done
+   echo 'sum'
+   echo $sum
+if [ $sum = 250500 ]
+then
+   exit 0
+else 
+   exit 1
+fi
